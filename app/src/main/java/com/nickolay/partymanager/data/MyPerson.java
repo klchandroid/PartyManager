2ad9e169214a3f88dea94f8361977a2b32134100
Nickolay Cherniaev <biblbrox@yandex.ru>
2018-11-12 14:14:11 +0300

    No Fragments No DB v 0.1

--- app/src/main/java/com/nickolay/partymanager/data/MyPerson.java
+++ app/src/main/java/com/nickolay/partymanager/data/MyPerson.java
@@ -1,14 +1,20 @@
 package com.nickolay.partymanager.data;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 
 public class MyPerson extends MyItem {
+    public final String ACTIVITY_TITLE = "Редактирование человека";
+    public final String ACTIVITY_LIST_TITLE = "Учаснеги";
     private Set<MySpendings> inCharge = new HashSet<>();
 
     public MyPerson(String name, double summa, Set<MySpendings> inCharge) {
@@ -20,7 +26,11 @@ public class MyPerson extends MyItem {
     }
 
     public Set<MySpendings> getInCharge() {
-        return Collections.unmodifiableSet(inCharge);
+        return inCharge;
+    }
+    public  void setInCharge(Set inCharge)
+    {
+        this.inCharge = inCharge;
     }
 
     public MyItem removeInChargeElement(MySpendings element)
@@ -47,5 +57,42 @@ public class MyPerson extends MyItem {
     @Override
     public int hashCode() {
         return super.hashCode();
+    }
+
+    public static  List<MyPerson> calculateItog(List<MyPerson> persons, List<MySpendings> spendings)
+    {
+        Map<MySpendings, Double> sPerPerson = new HashMap<>();
+        List<MyPerson> itogList = new ArrayList<>();
+        for(MySpendings ms: spendings)
+        {
+            int i = 0;
+            for(MyPerson mp: persons)
+            {
+                if(mp.getInCharge().contains(ms))
+                {
+                    i++;
+                }
+            }
+            Log.d("ITOG", ms.getName() + " " + ms.getSumma() + " " + i );
+            sPerPerson.put(ms, ms.getSumma()/i);
+        }
+        for(MyPerson mp: persons)
+        {
+            double itog = 0d;
+            for(MySpendings ms: mp.getInCharge())
+            {
+                try {
+                    itog = itog + sPerPerson.get(ms);
+                } catch (NullPointerException e)
+                {
+                    continue;
+                }
+            }
+            MyPerson pItog = new MyPerson();
+            pItog.setName(mp.getName());
+            pItog.setSumma(itog);
+            itogList.add(pItog);
+        }
+        return itogList;
     }
 }
